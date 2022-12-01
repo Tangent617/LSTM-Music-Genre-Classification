9f4d3c3a9ac471f35fc95264484b6aef917405af
Tangent617 <1155177900@link.cuhk.edu.hk>
2022-12-01 23:36:43 +0800

    Update lstm_genre_classifier_keras.py
    change activation function

--- lstm_genre_classifier_keras.py
+++ lstm_genre_classifier_keras.py
@@ -83,9 +83,8 @@ input_shape = (genre_features.train_X.shape[1], genre_features.train_X.shape[2])
 print("Build LSTM RNN model ...")
 model = Sequential()
 
-model.add(LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True, input_shape=input_shape))
-model.add(LSTM(units=64,  dropout=0.05, recurrent_dropout=0.35, return_sequences=True))#return_sequences=True if you have mor elayers
-model.add(LSTM(units=32,  dropout=0.05, recurrent_dropout=0.35, return_sequences=False))#added
+model.add(LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True, input_shape=input_shape, activation='relu'))
+model.add(LSTM(units=32,  dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
 model.add(Dense(units=genre_features.train_Y.shape[1], activation="softmax"))
 
 print("Compiling ...")
